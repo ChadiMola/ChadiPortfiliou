9a0c987407d77f2796709c2f81947b27c3a4f8c9
ChadiMola <114395884+ChadiMola@users.noreply.github.com>
2023-01-02 22:11:15 +0100

    v0.1

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -3,7 +3,7 @@ import './NavBar.css'
 import {TiHomeOutline} from 'react-icons/ti'
 import {HiOutlineUser} from 'react-icons/hi'
 import {BiBookAlt} from 'react-icons/bi'
-import {MdOutlineDesignServices} from 'react-icons/md'
+//import {MdOutlineDesignServices} from 'react-icons/md'
 import {MdOutlinePermContactCalendar} from 'react-icons/md'
 import { useState, useEffect } from "react";
 
@@ -14,18 +14,20 @@ function NavBar() {
     const homeElement = document.querySelector('#home');
     const aboutElement = document.querySelector('#about');
     const experienceElement = document.querySelector('#experience');
-    const servicesElement = document.querySelector('#services');
+   /* const servicesElement = document.querySelector('#services');*/
     const contactElement = document.querySelector('#contact');
     const scrollPosition = window.scrollY;
-    if (scrollPosition >= homeElement.offsetTop && scrollPosition < aboutElement.offsetTop) {
+    
+
+    if (scrollPosition <= homeElement.offsetTop && scrollPosition < aboutElement.offsetTop) {
       setActive("#home");
-    } else if (scrollPosition >= aboutElement.offsetTop && scrollPosition < experienceElement.offsetTop) {
+    } else if (scrollPosition <= aboutElement.offsetTop && scrollPosition < experienceElement.offsetTop) {
       setActive("#about");
-    } else if (scrollPosition >= experienceElement.offsetTop && scrollPosition < servicesElement.offsetTop) {
+    } else if (scrollPosition <= experienceElement.offsetTop && scrollPosition < contactElement.offsetTop) {
       setActive("#experience");
-    } else if (scrollPosition >= servicesElement.offsetTop && scrollPosition < contactElement.offsetTop) {
+    } /*else if (scrollPosition >= servicesElement.offsetTop && scrollPosition < contactElement.offsetTop) {
       setActive("#services");
-    } else if (scrollPosition >= contactElement.offsetTop) {
+    } */else if (scrollPosition <= contactElement.offsetTop) {
       setActive("#contact");
     }
   }
@@ -39,10 +41,10 @@ function NavBar() {
 
   return (
   <nav>
-    <a href="#" onClick={() => setActive("#home")} className={active === '#home' ? 'active' : ''}><TiHomeOutline/></a>
+    <a href="#home" onClick={() => setActive("#home")} className={active === '#home' ? 'active' : ''}><TiHomeOutline/></a>
     <a href="#about" onClick={() => setActive("#about")} className={active === "#about" ? 'active': ""} ><HiOutlineUser/></a>
     <a href="#experience" onClick={() => setActive("#experience")} className={active === "#experience" ? 'active': ""} ><BiBookAlt/></a>
-    <a href="#servecis" onClick={() => setActive("#servecis")} className={active === "#servecis" ? 'active': ""} ><MdOutlineDesignServices/></a>
+    {/*<a href="#servecis" onClick={() => setActive("#servecis")} className={active === "#servecis" ? 'active': ""} ><MdOutlineDesignServices/></a>*/}
     <a href="#contact"onClick={() => setActive("#contact")} className={active === "#contact" ? 'active': ""}  ><MdOutlinePermContactCalendar/></a>
   </nav>
   );
